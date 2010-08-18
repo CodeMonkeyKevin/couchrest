541c4b69f43c118e204fc61f1ad765ed4620e274
Kevin Patel <dcobra@gmail.com>
2010-08-18 18:14:13 -0400

    Ruby 1.9.2 proper require added and took benchmarks from Sam's fast branch.

--- benchmarks/haza.rb
+++ benchmarks/haza.rb
@@ -1,67 +1,82 @@
 require 'rubygems'
-require '../lib/couchrest'
 require 'benchmark'
 
-iterations = 15
+if RUBY_VERSION =~ /1.9/
+  $: << File.dirname(__FILE__)
+  require_relative '../lib/couchrest'
+else
+  require '../lib/couchrest'
+end
+
+@iterations = 100
 
 @payload = {:key => 'value', 'another key' => 'another value'}
-db_url = "http://admin:pass@127.0.0.1:5984/testdb"
+@db_url = "http://127.0.0.1:5984/testdb"
 
-CouchRest.adapter = :restclient
-@db = CouchRest.database(db_url)
-@db.recreate!
-@last_doc = nil
-
-Benchmark.bm do |x|
-  x.report("RestClient-PUT") do
-    iterations.times do
-      @last_doc = @db.save_doc(@payload)
-    end
-  end
-
-  x.report(" RestClient-GET") do
-    iterations.times do
-      @db.get(@last_doc['id'])
-    end
-  end
-
-  x.report(" RestClient-POST") do
-    iterations.times do
-      @db.bulk_save([
-          {"wild" => "and random"},
-          {"mild" => "yet local"},
-          {"another" => ["set","of","keys"]}
-        ])
-    end
-  end
-end
-
-CouchRest.adapter = :curb
-@db = CouchRest.database(db_url)
-@db.recreate!
-@last_doc = nil
-
-Benchmark.bm do |x|
+def run_curb
   CouchRest.adapter = :curb
-  x.report("       Curb-PUT") do
-    iterations.times do
-      @last_doc = @db.save_doc(@payload)
-    end
-  end
-  
-  x.report("       Curb-GET") do
-    iterations.times do
-      @db.get(@last_doc['id'])
-    end
-  end
+  @db = CouchRest.database(@db_url)
+  @db.recreate!
+  @last_doc = nil
 
-  x.report("      Curb-POST") do
-    iterations.times do
-      @db.bulk_save([
-          {"wild" => "and random"},
-          {"mild" => "yet local"},
-          {"another" => ["set","of","keys"]}
-        ])
+  Benchmark.bm do |x|
+    CouchRest.adapter = :curb
+    x.report("       Curb-PUT") do
+      @iterations.times do
+        @last_doc = @db.save_doc(@payload)
+      end
+    end
+    
+    x.report("       Curb-GET") do
+      @iterations.times do
+        @db.get(@last_doc['id'])
+      end
+    end
+
+    x.report("      Curb-POST") do
+      @iterations.times do
+        @db.bulk_save([
+            {"wild" => "and random"},
+            {"mild" => "yet local"},
+            {"another" => ["set","of","keys"]}
+          ])
+      end
     end
   end
 end
+
+def run_restclient
+  CouchRest.adapter = :restclient
+  @db = CouchRest.database(@db_url)
+  @db.recreate!
+  @last_doc = nil
+
+  Benchmark.bm do |x|
+    x.report(" RestClient-PUT") do
+      @iterations.times do
+        @last_doc = @db.save_doc(@payload)
+      end
+    end
+
+    x.report(" RestClient-GET") do
+      @iterations.times do
+        @db.get(@last_doc['id'])
+      end
+    end
+
+    x.report("RestClient-POST") do
+      @iterations.times do
+        @db.bulk_save([
+            {"wild" => "and random"},
+            {"mild" => "yet local"},
+            {"another" => ["set","of","keys"]}
+          ])
+      end
+    end
+  end
+end
+
+run_curb
+run_curb
+run_restclient
+run_restclient
